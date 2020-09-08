a8c091db2717922e216811421a7a326c22f03662
Grant Williams <greatgrant116@gmail.com>
2020-09-08 22:22:34 +0100

    Added SplashScreenActivity

---
+++ app/src/main/java/com/gralliams/gads2020leaderboard/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.gralliams.gads2020leaderboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    public static final int SPLASH_TIMEOUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+            finish();
+        }, SPLASH_TIMEOUT);
+    }
+}
