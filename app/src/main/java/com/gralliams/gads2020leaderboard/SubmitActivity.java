65274e607a58112acc4566d782ba47974504379a
Grant Williams <greatgrant116@gmail.com>
2020-09-09 19:56:13 +0100

    Added layout for permission dialog

--- app/src/main/java/com/gralliams/gads2020leaderboard/SubmitActivity.java
+++ app/src/main/java/com/gralliams/gads2020leaderboard/SubmitActivity.java
@@ -1,5 +1,8 @@
 package com.gralliams.gads2020leaderboard;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -17,8 +20,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.util.Objects;
 
 public class SubmitActivity extends AppCompatActivity {
 
@@ -28,6 +35,10 @@ public class SubmitActivity extends AppCompatActivity {
     private EditText mName;
     private EditText mSurname;
     private Retrofit mRetrofit;
+    private String mUserName;
+    private String mUserEmail;
+    private String mUserGitLink;
+    private String mUserSurname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,40 +60,67 @@ public class SubmitActivity extends AppCompatActivity {
         mName = findViewById(R.id.edittext_name);
         mSurname = findViewById(R.id.edittext_surname);
 
-        String userName = mName.getText().toString();
-        String userEmail = mEmail.getText().toString();
-        String userGitLink = mGitLink.getText().toString();
-        String userSurname = mSurname.getText().toString();
+        mUserName = mName.getText().toString();
+        mUserEmail = mEmail.getText().toString();
+        mUserGitLink = mGitLink.getText().toString();
+        mUserSurname = mSurname.getText().toString();
 
-        if (!userEmail.trim().isEmpty() && !userGitLink.trim().isEmpty() && !userName.trim().isEmpty()
-                && !userSurname.trim().isEmpty()) {
-
-            LeaderboardApiInterface leaderboardApiInterface = createRetrofitFormInstance().create(LeaderboardApiInterface.class);
-            Call<Void> postCall = leaderboardApiInterface.submitProject(userEmail, userName, userSurname, userGitLink);
-            postCall.enqueue(new Callback<Void>() {
-                @Override
-                public void onResponse(Call<Void> call, Response<Void> response) {
-                    clearfields();
-                    if (response.isSuccessful()) {
-
-                        Toast.makeText(SubmitActivity.this, "Success! " + response.code(), Toast.LENGTH_SHORT).show();
-                        Log.v("TAG", response.message() + response.code());
-                    } else {
-                        Toast.makeText(SubmitActivity.this, "failed! " + response.message() + " " +
-                                        response.code(),
-                                Toast.LENGTH_SHORT).show();
-                    }
-                }
-
-                @Override
-                public void onFailure(Call<Void> call, Throwable t) {
-                    Log.e("TAG", "Network request failed " + t.toString());
-                    Toast.makeText(SubmitActivity.this, t.toString(), Toast.LENGTH_SHORT).show();
-                }
-            });
+        if (!mUserEmail.trim().isEmpty() && !mUserGitLink.trim().isEmpty() && !mUserName.trim().isEmpty()
+                && !mUserSurname.trim().isEmpty()) {
+            showPermissionDialog();
         } else {
             Toast.makeText(this, "Please input text", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void showResponse() {
+        LeaderboardApiInterface leaderboardApiInterface = createRetrofitFormInstance().create(LeaderboardApiInterface.class);
+        Call<Void> postCall = leaderboardApiInterface.submitProject(mUserEmail, mUserName, mUserSurname, mUserGitLink);
+        postCall.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                clearfields();
+                if (response.isSuccessful()) {
+                    Toast.makeText(SubmitActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                    // showSuccessDialog();
+                    Log.v("TAG", response.message() + response.code());
+                } else {
+                    Toast.makeText(SubmitActivity.this, "failed! " + response.message() + " " +
+                                    response.code(),
+                            Toast.LENGTH_SHORT).show();
+                    //showFailureDialog();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Log.e("TAG", "Network request failed " + t.toString());
+                Toast.makeText(SubmitActivity.this, t.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    private void showPermissionDialog() {
+        final Dialog alertDialog = new Dialog(SubmitActivity.this);
+        alertDialog.setContentView(R.layout.dialog_permission);
+        Objects.requireNonNull(alertDialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        ImageView cancelPermission = alertDialog.findViewById(R.id.imageView_no);
+        Button givePermission = alertDialog.findViewById(R.id.button_yes);
+        //TO DO IMPLEMENT IN ON START
+        alertDialog.show();
+        cancelPermission.setOnClickListener(view -> {
+            //implement in onStop
+            alertDialog.dismiss();
+        });
+
+        givePermission.setOnClickListener(view -> showResponse());
+    }
+
+    private void showFailureDialog() {
+    }
+
+    private void showSuccessDialog() {
 
     }
 
